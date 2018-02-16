f7e2264fe3f16b0fcf6093b6a2e15afce552d517
lemonde-L <35504551+lemonde-L@users.noreply.github.com>
2018-02-15 18:10:53 -0800

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Project.Models;
+
+namespace Project.Controllers
+{
+    public class HomeController : Controller
+    {
+        private CommunityAssist2017Entities db = new CommunityAssist2017Entities();
+
+        public ActionResult Index()
+        {
+            return View(db.GrantTypes.ToList());
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Controllers/RegistrationController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Project.Models;
+
+namespace Project.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        private CommunityAssist2017Entities db = new CommunityAssist2017Entities();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(
+            [Bind(Include =
+            "LastName, FirstName, Email, Phone, PlainPasswrod,"+
+            "Apartment, Street, City, State, Zipcode")
+            ]NewPerson np)
+        {
+            int result = db.usp_Register(
+                np.LastName,
+                np.FirstName,
+                np.Email,
+                np.PlainPassword,
+                np.Apartment,
+                np.Street,
+                np.City,
+                np.State,
+                np.Zipcode,
+                np.Phone);
+
+            Message m = new Message();
+
+            if (result == -1)
+            {
+                m.MessageText = "Sorry, but something seems to have gone wrong with the registration.";
+            }
+            else
+            {
+                m.MessageText = "Thanks for registering.";
+            }
+
+            return View("Result", m);
+        }
+    }
+}
